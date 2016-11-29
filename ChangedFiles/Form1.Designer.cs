e0fb0b699465b1f7b32064f036292df0b14594fe
Z003KJVZ <a>
2016-11-29 10:44:43 +0300

    grid view changes

--- ChangedFiles/Form1.Designer.cs
+++ ChangedFiles/Form1.Designer.cs
@@ -30,16 +30,21 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastChange = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(132, 10);
+            this.button1.Location = new System.Drawing.Point(139, 10);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(251, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "List All Files";
+            this.button1.Text = "Start Searching and List All Files";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -48,29 +53,73 @@
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.dateTimePicker1.Location = new System.Drawing.Point(13, 13);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(87, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(120, 20);
             this.dateTimePicker1.TabIndex = 1;
             // 
-            // listBox1
+            // dataGridView1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 39);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(581, 199);
-            this.listBox1.TabIndex = 3;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleVertical;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FileName,
+            this.LastChange});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1009, 215);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // FileName
+            // 
+            this.FileName.HeaderText = "File Name";
+            this.FileName.Name = "FileName";
+            this.FileName.ReadOnly = true;
+            this.FileName.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.FileName.Width = 700;
+            // 
+            // LastChange
+            // 
+            this.LastChange.HeaderText = "Last Change";
+            this.LastChange.Name = "LastChange";
+            this.LastChange.ReadOnly = true;
+            this.LastChange.Width = 250;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(396, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Please wait some time until it finishes searching after clicking button";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "You can click file name to open directory";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(605, 362);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(1033, 291);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -78,7 +127,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastChange;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ChangedFiles/Form1.cs
+++ ChangedFiles/Form1.cs
@@ -16,32 +16,71 @@ namespace ChangedFiles
         public Form1()
         {
             InitializeComponent();
+            label2.Visible = false;
         }
+
+        void OpenFile(string path)
+        {
+            string filePath = @dataGridView1.CurrentCell.Value.ToString();
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Please click on File Name");
+                return;
+            }
+            string argument = "/select, \"" + filePath + "\"";
+            System.Diagnostics.Process.Start("explorer.exe", argument);
+        }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            listBox1.Items.Clear();
+            dataGridView1.Rows.Clear();
             FolderBrowserDialog fbd = new FolderBrowserDialog();
 
             DialogResult result = fbd.ShowDialog();
+
+            if (result == DialogResult.Cancel)
+            {
+                MessageBox.Show("Cancel Clicked");
+                return;
+            }
 
             if (string.IsNullOrWhiteSpace(fbd.SelectedPath))
             {
                 MessageBox.Show(fbd.SelectedPath);
                 return;
             }
+
             string[] files = Directory.GetFiles(fbd.SelectedPath, "*.*", SearchOption.AllDirectories);
+
+            FileInfo oFileInfo;
+            DateTime dtCreationTime;
+            int i = 0;
 
             // Display all the files.
             foreach (string file in files)
             {
-                FileInfo oFileInfo = new FileInfo(file);
-                DateTime dtCreationTime = oFileInfo.CreationTime;
+                oFileInfo = new FileInfo(file);
+                dtCreationTime = oFileInfo.CreationTime;
                 if (dtCreationTime > dateTimePicker1.Value)
                 {
-                    listBox1.Items.Add(file + "-" + dtCreationTime.ToShortDateString());
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[i].Cells[0].Value = file;
+                    dataGridView1.Rows[i].Cells[1].Value = string.Concat(dtCreationTime.ToLongDateString(),dtCreationTime.ToLongTimeString());
+                    i++;
                 }
-                
+            }
+            label2.Visible = true;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows.Count> 0 && e.RowIndex != -1)
+            {
+                if (dataGridView1.CurrentCell != null && dataGridView1.CurrentCell.Value != null)
+                {
+                    OpenFile(dataGridView1.CurrentCell.Value.ToString());
+                }
             }
         }
     }
